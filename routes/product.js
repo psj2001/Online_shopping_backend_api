0d0d78e1d956e942d7b45e10389550259b635929
pranavcasa <pranav@casakeyrealestate.ae>
2025-01-20 17:46:37 +0400

    Add new route for retrieving products by category and simplify response structure for popular products

--- routes/product.js
+++ routes/product.js
@@ -37,7 +37,7 @@ productRouter.get('/api/popular-products', async (req, res) => {
         if (!product || product.length === 0) {
             return res.status(404).json({ error: 'No popular product found' });
         } else {
-            res.status(200).json({ product });
+            res.status(200).json(product);
         }
     } catch (e) {
         res.status(500).json({ error: 'Internal server error', details: e.message });
@@ -56,4 +56,23 @@ productRouter.get('/api/recommend-products', async (req, res) => {
         res.status(500).json({ error: 'Internal server error', details: e.message });
     }
 });
+// New route for retrieving Products by category
+productRouter.get('/api/products-by-category/:category', async (req, res) => {
+    try {
+        const { category } = req.params;
+        const products = await Product.find({ category });
+
+        if (!products || products.length === 0) {
+            // No products found
+            return res.status(404).json({ msg: "Products not found" });
+        }
+
+        // Products found
+        return res.status(200).json(products);
+    } catch (e) {
+        // Handle server errors
+        return res.status(500).json({ error: e.message });
+    }
+});
+
 module.exports = productRouter;
